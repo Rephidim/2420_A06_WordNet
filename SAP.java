be199d3161d8b935f28362d59f7b18bc4dc75044
Rephidim <mcpvy@hotmail.com>
2018-04-21 13:16:04 -0600

    Update SAP.java
    it works more or less as the assignment requires.

--- SAP.java
+++ SAP.java
@@ -1,6 +1,8 @@
 package main;
 
-import edu.princeton.cs.algs4.DepthFirstDirectedPaths;
+import java.util.ArrayList;
+
+import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.DirectedCycle;
 import edu.princeton.cs.algs4.Stack;
@@ -47,24 +49,32 @@ public class SAP {
 	 *         or -1 if no such path.
 	 */
 	public int length(int v, int w) {
-		if (v < 0 || v > G.V() -1) throw new IndexOutOfBoundsException("Vertex v is not between 0 and G.V() -1");
-		if (w < 0 || w > G.V() -1) throw new IndexOutOfBoundsException("Vertex w is not between 0 and G.V() -1");
+		throwOOB(v, w);
 		if (!isRootedDAG()) throw new IllegalArgumentException("Use a rooted DAG");
+		
 		int root = 0;
 		int L = -1;
 		for (int i = 0; i < G.V(); i++) {
 			if (!G.adj(i).iterator().hasNext()) root = i;
 		}
-		Stack<Integer> vStack = (Stack<Integer>)new DepthFirstDirectedPaths(G,v).pathTo(root);
-		Stack<Integer> wStack = (Stack<Integer>)new DepthFirstDirectedPaths(G,w).pathTo(root);
+		Stack<Integer> vStack = (Stack<Integer>)new BreadthFirstDirectedPaths(G,v).pathTo(root);
+		Stack<Integer> wStack = (Stack<Integer>)new BreadthFirstDirectedPaths(G,w).pathTo(root);
+		Stack<Integer> vStack2 = new Stack<Integer>();
+		for (int i = 0; i < vStack.size(); i++) {
+			vStack2.push(vStack.pop());
+		}
+		Stack<Integer> wStack2 = new Stack<Integer>();
+		for (int i = 0; i < wStack.size(); i++) {
+			wStack2.push(wStack.pop());
+		}
 		
-		while (!vStack.isEmpty()) {
-			if (vStack.peek() == wStack.peek()) {
-				vStack.pop();
-				wStack.pop();
+		while (!vStack2.isEmpty()) {
+			if (vStack2.peek().equals(wStack2.peek())) {
+				vStack2.pop();
+				wStack2.pop();
 			} else break;
 		}
-		L = vStack.size() + wStack.size();
+		L = vStack2.size() + wStack2.size();
 		return L;
 	}
 	
@@ -77,22 +87,29 @@ public class SAP {
 	 * @throws IndexOutOfBoundsException
 	 */
 	public int ancestor(int v, int w) {
-		if (v < 0 || v > G.V() -1) throw new IndexOutOfBoundsException("Vertex v is not between 0 and G.V() -1");
-		if (w < 0 || w > G.V() -1) throw new IndexOutOfBoundsException("Vertex w is not between 0 and G.V() -1");
+		throwOOB(v, w);
 		
 		int root = 0;
 		for (int i = 0; i < G.V(); i++) {
 			if (!G.adj(i).iterator().hasNext()) root = i;
 		}
-		Stack<Integer> vStack = (Stack<Integer>)new DepthFirstDirectedPaths(G,v).pathTo(root);
-		Stack<Integer> wStack = (Stack<Integer>)new DepthFirstDirectedPaths(G,w).pathTo(root);
+		Stack<Integer> vStack = (Stack<Integer>)new BreadthFirstDirectedPaths(G,v).pathTo(root);
+		Stack<Integer> wStack = (Stack<Integer>)new BreadthFirstDirectedPaths(G,w).pathTo(root);
+		Stack<Integer> vStack2 = new Stack<Integer>();
+		for (int i = 0; i < vStack.size(); i++) {
+			vStack2.push(vStack.pop());
+		}
+		Stack<Integer> wStack2 = new Stack<Integer>();
+		for (int i = 0; i < wStack.size(); i++) {
+			wStack2.push(wStack.pop());
+		}
 		
 		int ca = -1;
-		while (!vStack.isEmpty()) {
-			if (vStack.peek() == wStack.peek()) {
-				ca = vStack.peek();
-				vStack.pop();
-				wStack.pop();
+		while (!vStack2.isEmpty()) {
+			if ((vStack2.peek().equals(wStack2.peek()))) {
+				ca = vStack2.peek();
+				vStack2.pop();
+				wStack2.pop();
 			} else break;
 		}
 		return ca;
@@ -108,7 +125,18 @@ public class SAP {
 	 */
 	public int length(Iterable<Integer> v, Iterable<Integer> w) {
 		if (v == null || w == null) throw new NullPointerException("Args cannot be null");
-		return 0;
+		for (int i : v) throwOOB(i);
+		for (int i : w) throwOOB(i);
+		
+		int minDistance = Integer.MAX_VALUE;
+		int l;
+		for (int vItem : v) {
+			for (int wItem : w) {
+				l = length(vItem, wItem);
+				if ( l < minDistance) minDistance = l;
+			}
+		}
+		return minDistance;
 	}
 	
 	/**
@@ -120,7 +148,43 @@ public class SAP {
 	 */
 	public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
 		if (v == null || w == null) throw new NullPointerException("Args cannot be null");
-		return 0;
+		for (int i : v) throwOOB(i);
+		for (int i : w) throwOOB(i);
+		
+		BreadthFirstDirectedPaths vbfs = new BreadthFirstDirectedPaths(G, v);
+		BreadthFirstDirectedPaths wbfs = new BreadthFirstDirectedPaths(G, w);
+		
+		ArrayList<Integer> ancestors = new ArrayList<Integer>();
+		
+		for (int i = 0; i < G.V(); i ++) {
+			if (vbfs.hasPathTo(i) && wbfs.hasPathTo(i)) {
+				ancestors.add(i);
+			}
+		}
+		
+		int minDistance = Integer.MAX_VALUE;
+		int minAncestor = -1;
+		for (int n : ancestors) {
+			int dist = vbfs.distTo(n) + wbfs.distTo(n);
+			if (dist < minDistance) {
+				minDistance = dist;
+				minAncestor = n;
+			}
+		}
+		
+		return minAncestor;
+	}
+	
+	/**
+	 * @param v
+	 * @param w
+	 */
+	private void throwOOB(int v, int w) {
+		if (v < 0 || v > G.V() -1) throw new IndexOutOfBoundsException("Vertex v is not between 0 and G.V() -1");
+		if (w < 0 || w > G.V() -1) throw new IndexOutOfBoundsException("Vertex w is not between 0 and G.V() -1");
+	}
+	private void throwOOB(int v) {
+		if (v < 0 || v > G.V() -1) throw new IndexOutOfBoundsException("Vertex v is not between 0 and G.V() -1");
 	}
 	
 	public static void main(String[] args) {
